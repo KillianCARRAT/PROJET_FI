ea47e5b7ec213854883d25e945157bfd40152262
Marin Chesneau <144822096+Marin-univ@users.noreply.github.com>
2024-10-22 16:05:41 +0200

    Resolution problemes.sql
    résolution de quelques erreurs sur le trigger PlaceEnLogesSuffisantes :
    
    - ajout du delimiter
    - changement du ON EACH ROW en FOR EACH ROW

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE TRIGGER PlaceEnLogesSuffisantes BEFORE INSERT ON RESERVATION  ON EACH ROW
+delimiter
+CREATE OR REPLACE TRIGGER PlaceEnLogesSuffisantes BEFORE INSERT ON RESERVATION FOR EACH ROW
 begin
     declare nbArtiste;
     declare nbLoges;
@@ -10,3 +11,4 @@ begin
         signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
     end if;
 end |
+delimiter ;
