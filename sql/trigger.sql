3d4555ce37aa6213213d173e5ab4827746f09984
Noa Fonteny <o22305140@or-iut-i204-f03.iut45.univ-orleans.fr>
2024-10-22 16:05:12 +0200

    résolution problème trigger 1

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,22 +1,22 @@
-CREATE OR REPLACE TRIGGER ReservationSalleMemeMoment BEFORE INSERT ON RESERVATION ON EACH ROW
+delimiter |
+CREATE OR REPLACE TRIGGER ReservationSalleMemeMoment BEFORE INSERT ON RESERVATION FOR EACH ROW
 begin
     declare mess VARCHAR(500);
     declare debut date;
     declare fin date;
     declare fini boolean default false;
     declare lesHeures cursor for 
-        select dateR, ADDTIME(dateR,dureeR) FROM RESERVATION WHERE idS=new.idS;
+        select dateR, ADDDATE(dateR,dureeR) FROM RESERVATION WHERE idS=new.idS;
     declare continue handler for not found set fini = true;
     open lesHeures;
     while not fini do
         fetch lesHeures into debut,fin;
-        if new.dateR>debut AND new.dateR<fin then
+        if NEW.dateR < fin AND ADDDATE(NEW.dateR, INTERVAL NEW.dureeR DAY) > debut then
             close lesHeures;
-            set mess = concat ("réservation impossible", new.idR);
-            signal SQLSTATE ’45000’ set MESSAGE_TEXT = mess;
+            set mess = concat ("réservation impossible ", new.idR);
+            signal SQLSTATE '45000' set MESSAGE_TEXT = mess;
         end if;
     end while;
     close lesHeures;
 end |
-
-INSERT into 
+delimiter ;
