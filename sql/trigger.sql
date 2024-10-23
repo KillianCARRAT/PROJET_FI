ec4830fa2ea0b422a50c6d6a6801821a5927bc87
Noa Fonteny <o22305140@or-iut-i204-f03.iut45.univ-orleans.fr>
2024-10-23 15:52:04 +0200

    trigger fini et testé

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -5,6 +5,7 @@ begin
     declare mess VARCHAR(500);
     declare debut date;
     declare fin date;
+    declare probleme boolean default false;
     declare fini boolean default false;
     declare lesHeures cursor for 
         select heureArrive, ADDDATE(debutConcert,dureeConcert) FROM CONCERT WHERE idS=new.idS;
@@ -12,7 +13,13 @@ begin
     open lesHeures;
     while not fini do
         fetch lesHeures into debut,fin;
-        if NEW.heureArrive < fin AND ADDDATE(NEW.heureArrive, INTERVAL NEW.dureeConcert DAY) > debut then
+        if NEW.heureArrive>debut OR ADDDATE(NEW.debutConcert,NEW.dureeConcert)<fin then
+            set probleme:=1;
+        end if;
+        if NEW.heureArrive<debut AND ADDDATE(NEW.debutConcert,NEW.dureeConcert)>fin then
+            set probleme:=1;
+        end if;
+        if probleme then
             close lesHeures;
             set mess = concat ("réservation impossible pour la réservation numéro ", new.idC);
             signal SQLSTATE '45000' set MESSAGE_TEXT = mess;
