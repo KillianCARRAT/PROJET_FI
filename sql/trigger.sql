6ae833703fce194338afb3661fc86e1fa09fd855
DanCoquerant <dan.coquerant@gmail.com>
2024-10-24 12:12:47 +0200

    ajout du cas ou concert est sur 2 jours

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -3,31 +3,47 @@
 delimiter |
 CREATE OR REPLACE TRIGGER pasDeCheuvauchementConcertsInsert before insert on CONCERT for each row
 begin
-    DECLARE heureAvant INT;
-    DECLARE dureeAvant INT;
-    DECLARE heureApres INT;
-    DECLARE mes VARCHAR(100) default '';
+    DECLARE heureAvant TIME;
+    DECLARE dureeAvant TIME;
+    DECLARE dateAvant DATE;
+    DECLARE heureApres TIME;
+    DECLARE dateApres DATE;
+    DECLARE dateTimeAvant DATETIME;
+    DECLARE dateTimeApres DATETIME;
+    DECLARE dateTimeNew DATETIME;
+    DECLARE timeDiffr INT;
+    DECLARE temp VARCHAR(100) default '';
+    DECLARE mes VARCHAR(1000) default '';
 
     -- recupèration du premier concert avant celui ajouter
-    SELECT debutConcert,dureeConcert INTO heureAvant, dureeAvant FROM CONCERT
-    WHERE dateC = new.dateC AND debutConcert < new.debutConcert AND idG = new.idG
-    ORDER BY debutConcert DESC LIMIT 1;
+    SELECT debutConcert,dureeConcert,dateC INTO heureAvant, dureeAvant, dateAvant FROM CONCERT
+    WHERE debutConcert <= new.debutConcert AND dateC=new.dateC AND idG = new.idG OR idG = new.idG AND dateC < new.dateC
+    ORDER BY dateC DESC,debutConcert DESC LIMIT 1;
 
     -- recupèration du premier concert après celui ajouter
-    SELECT debutConcert INTO heureApres FROM CONCERT
-    WHERE dateC = new.dateC AND debutConcert > new.debutConcert AND idG = new.idG
-    ORDER BY debutConcert LIMIT 1;
+    SELECT debutConcert,dateC INTO heureApres,dateApres FROM CONCERT
+    WHERE debutConcert >= new.debutConcert AND dateC=new.dateC AND idG = new.idG OR idG = new.idG AND dateC > new.dateC 
+    ORDER BY dateC,debutConcert LIMIT 1;
 
-    -- vérification que le concert d'avant se termine avant le debut du concert ajouté
-    IF dureeAvant IS NOT NULL AND heureAvant IS NOT NULL AND new.debutConcert < heureAvant + dureeAvant THEN
-        SET mes = concat(mes,"Ajout impossible car il existe déjà un concert précédent sur le créneau ", SEC_TO_TIME(TIME_TO_SEC(new.debutConcert)), "-", SEC_TO_TIME(TIME_TO_SEC(new.debutConcert) + TIME_TO_SEC(new.dureeConcert)),".");
-        signal SQLSTATE '45000' SET MESSAGE_TEXT = mes ;
+    IF dateAvant IS NOT NULL THEN
+        SET dateTimeAvant = STR_TO_DATE(concat(YEAR(dateAvant),"-",MONTH(dateAvant),"-",DAY(dateAvant)," ",heureAvant),"%Y-%m-%d %H:%i:%s");
+        SET dateTimeNew = STR_TO_DATE(concat(YEAR(new.dateC),"-",MONTH(new.dateC),"-",DAY(new.dateC)," ",new.debutConcert),"%Y-%m-%d %H:%i:%s");
+        SET timeDiffr = TIMESTAMPDIFF(SECOND,dateTimeAvant,dateTimeNew);
+        IF ABS(timeDiffr) < TIME_TO_SEC(dureeAvant) THEN
+            SET mes = concat(mes,"Ajout impossible car il existe déjà un concert précédent sur le créneau ",dateTimeNew," - ",dateAvant," ",SEC_TO_TIME(timeDiffr));
+            signal SQLSTATE '45000' SET MESSAGE_TEXT = mes ;
+        end if;
     end if;
 
     -- vérification que le concert ajouté se termine avant que le concert suivant commence
-    IF heureApres IS NOT NULL AND new.debutConcert + new.dureeConcert > heureApres THEN
-        SET mes = concat(mes,"Ajout impossible car il existe déjà un concert suivant sur le créneau ", SEC_TO_TIME(TIME_TO_SEC(new.debutConcert)), "-", SEC_TO_TIME(TIME_TO_SEC(new.debutConcert) + TIME_TO_SEC(new.dureeConcert)),".");
-        signal SQLSTATE '45000' SET MESSAGE_TEXT = mes ;
+    IF dateApres IS NOT NULL THEN
+        SET dateTimeApres = STR_TO_DATE(concat(YEAR(dateApres),"-",MONTH(dateApres),"-",DAY(dateApres)," ",heureApres),"%Y-%m-%d %H:%i:%s");
+        SET dateTimeNew = STR_TO_DATE(concat(YEAR(new.dateC),"-",MONTH(new.dateC),"-",DAY(new.dateC)," ",new.debutConcert),"%Y-%m-%d %H:%i:%s");
+        SET timeDiffr = TIMESTAMPDIFF(SECOND,dateTimeNew,dateTimeApres);
+        IF ABS(timeDiffr) < TIME_TO_SEC(new.dureeConcert) THEN
+            SET mes = concat(mes,"Ajout impossible car il existe déjà un concert suivant sur le créneau. ",dateTimeNew," - ",dateTimeApres," ",SEC_TO_TIME(timeDiffr)," ",new.dureeConcert);
+            signal SQLSTATE '45000' SET MESSAGE_TEXT = mes ;
+        end if;
     end if;
 end|
 delimiter ;
@@ -36,31 +52,47 @@ delimiter ;
 delimiter |
 CREATE OR REPLACE TRIGGER pasDeCheuvauchementConcertsUpdate before update on CONCERT for each row
 begin
-    DECLARE heureAvant INT;
-    DECLARE dureeAvant INT;
-    DECLARE heureApres INT;
-    DECLARE mes VARCHAR(100) default '';
+    DECLARE heureAvant TIME;
+    DECLARE dureeAvant TIME;
+    DECLARE dateAvant DATE;
+    DECLARE heureApres TIME;
+    DECLARE dateApres DATE;
+    DECLARE dateTimeAvant DATETIME;
+    DECLARE dateTimeApres DATETIME;
+    DECLARE dateTimeNew DATETIME;
+    DECLARE timeDiffr INT;
+    DECLARE temp VARCHAR(100) default '';
+    DECLARE mes VARCHAR(1000) default '';
 
     -- recupèration du premier concert avant celui ajouter
-    SELECT debutConcert,dureeConcert INTO heureAvant, dureeAvant FROM CONCERT
-    WHERE dateC = new.dateC AND debutConcert < new.debutConcert AND idG = new.idG
-    ORDER BY debutConcert DESC LIMIT 1;
+    SELECT debutConcert,dureeConcert,dateC INTO heureAvant, dureeAvant, dateAvant FROM CONCERT
+    WHERE debutConcert <= new.debutConcert AND dateC=new.dateC AND idG = new.idG OR idG = new.idG AND dateC < new.dateC
+    ORDER BY dateC DESC,debutConcert DESC LIMIT 1;
 
     -- recupèration du premier concert après celui ajouter
-    SELECT debutConcert INTO heureApres FROM CONCERT
-    WHERE dateC = new.dateC AND debutConcert > new.debutConcert AND idG = new.idG
-    ORDER BY debutConcert LIMIT 1;
+    SELECT debutConcert,dateC INTO heureApres,dateApres FROM CONCERT
+    WHERE debutConcert >= new.debutConcert AND dateC=new.dateC AND idG = new.idG OR idG = new.idG AND dateC > new.dateC 
+    ORDER BY dateC,debutConcert LIMIT 1;
 
-    -- vérification que le concert d'avant se termine avant le debut du concert ajouté
-    IF dureeAvant IS NOT NULL AND heureAvant IS NOT NULL AND new.debutConcert < heureAvant + dureeAvant THEN
-        SET mes = concat(mes,"Ajout impossible car il existe déjà un concert précédent sur le créneau ", SEC_TO_TIME(TIME_TO_SEC(new.debutConcert)), "-", SEC_TO_TIME(TIME_TO_SEC(new.debutConcert) + TIME_TO_SEC(new.dureeConcert)),".");
-        signal SQLSTATE '45000' SET MESSAGE_TEXT = mes ;
+    IF dateAvant IS NOT NULL THEN
+        SET dateTimeAvant = STR_TO_DATE(concat(YEAR(dateAvant),"-",MONTH(dateAvant),"-",DAY(dateAvant)," ",heureAvant),"%Y-%m-%d %H:%i:%s");
+        SET dateTimeNew = STR_TO_DATE(concat(YEAR(new.dateC),"-",MONTH(new.dateC),"-",DAY(new.dateC)," ",new.debutConcert),"%Y-%m-%d %H:%i:%s");
+        SET timeDiffr = TIMESTAMPDIFF(SECOND,dateTimeAvant,dateTimeNew);
+        IF ABS(timeDiffr) < TIME_TO_SEC(dureeAvant) THEN
+            SET mes = concat(mes,"Ajout impossible car il existe déjà un concert précédent sur le créneau ",dateTimeNew," - ",dateAvant," ",SEC_TO_TIME(timeDiffr));
+            signal SQLSTATE '45000' SET MESSAGE_TEXT = mes ;
+        end if;
     end if;
 
     -- vérification que le concert ajouté se termine avant que le concert suivant commence
-    IF heureApres IS NOT NULL AND new.debutConcert + new.dureeConcert > heureApres THEN
-        SET mes = concat(mes,"Ajout impossible car il existe déjà un concert suivant sur le créneau ", SEC_TO_TIME(TIME_TO_SEC(new.debutConcert)), "-", SEC_TO_TIME(TIME_TO_SEC(new.debutConcert) + TIME_TO_SEC(new.dureeConcert)),".");
-        signal SQLSTATE '45000' SET MESSAGE_TEXT = mes ;
+    IF dateApres IS NOT NULL THEN
+        SET dateTimeApres = STR_TO_DATE(concat(YEAR(dateApres),"-",MONTH(dateApres),"-",DAY(dateApres)," ",heureApres),"%Y-%m-%d %H:%i:%s");
+        SET dateTimeNew = STR_TO_DATE(concat(YEAR(new.dateC),"-",MONTH(new.dateC),"-",DAY(new.dateC)," ",new.debutConcert),"%Y-%m-%d %H:%i:%s");
+        SET timeDiffr = TIMESTAMPDIFF(SECOND,dateTimeNew,dateTimeApres);
+        IF ABS(timeDiffr) < TIME_TO_SEC(new.dureeConcert) THEN
+            SET mes = concat(mes,"Ajout impossible car il existe déjà un concert suivant sur le créneau. ",dateTimeNew," - ",dateTimeApres," ",SEC_TO_TIME(timeDiffr)," ",new.dureeConcert);
+            signal SQLSTATE '45000' SET MESSAGE_TEXT = mes ;
+        end if;
     end if;
 end|
 delimiter ;
