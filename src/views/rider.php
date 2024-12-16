0a0a7d7fa92e45d5a5195815a9b07ccf7e144fb5
moucmoi <noatoimoi@gmail.com>
2024-12-16 14:33:10 +0100

    séparation js et php

--- src/views/rider.php
+++ src/views/rider.php
@@ -62,35 +62,6 @@ include 'head.php'; ?>
     </main>
     <?php include "basPage.php" ?>
 </body>
-<script>
-    function updateVisibility() {
-        const checkboxVehicule = document.getElementById('checkbox-vehicule');
-        const vehicule = document.getElementById('vehicule');
-
-        if (checkboxVehicule.checked) {
-            vehicule.style.visibility = "visible";
-        } else {
-            vehicule.style.visibility = "hidden";
-        }
-
-        const checkboxHotel = document.getElementById('checkbox-hotel');
-        const hotel = document.getElementById('hotel');
-
-        if (checkboxHotel.checked) {
-            hotel.style.visibility = "visible";
-        } else {
-            hotel.style.visibility = "hidden";
-        }
-    }
-
-
-    document.addEventListener('DOMContentLoaded', () => {
-        updateVisibility();
-
-
-        document.getElementById('checkbox-vehicule').addEventListener('change', updateVisibility);
-        document.getElementById('checkbox-hotel').addEventListener('change', updateVisibility);
-    });
-</script>
+<script src="../../public/assets/js/rider.js"></script>
 
 </html>
