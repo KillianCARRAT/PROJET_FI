732ddc702115aa0a32e871c49dc3690ea553e471
KillianCARRAT <143808966+KillianCARRAT@users.noreply.github.com>
2025-02-24 09:55:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,6 @@ require_once __DIR__ . '/src/config/config.php';
 require_once __DIR__ . '/src/controllers/Router.php';
 require_once __DIR__ . '/autoload.php';
 
-
-session_set_cookie_params([
-    'lifetime' => 0, // Session expire à la fermeture du navigateur
-    'path' => '/',
-    'domain' => $_SERVER['HTTP_HOST'], // Assure que le domaine est bien défini
-    'secure' => isset($_SERVER['HTTPS']), // Active uniquement en HTTPS
-    'httponly' => true, // Empêche l'accès au cookie via JavaScript
-    'samesite' => 'Lax' // Empêche certains problèmes de redirection
-]);
 session_start();
 
 use Src\Controllers\Router;
