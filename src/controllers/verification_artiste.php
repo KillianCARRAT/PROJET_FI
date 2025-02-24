4a1341697a67c0e0a04ec0653c4ab4a4fff903be
Fonteny Noa <143808893+moucmoi@users.noreply.github.com>
2025-02-24 17:19:58 +0100

    Creation spec (#120)
    * changement intermdiaireé
    
    * creation spec fini
    
    * Update problème-Crea-spec.php
    
    ---------
    
    Co-authored-by: KillianCARRAT <143808966+KillianCARRAT@users.noreply.github.com>

--- src/controllers/verification_artiste.php
+++ src/controllers/verification_artiste.php
@@ -1,44 +1,29 @@
 <?php
 
 // Connexion à la base de données
+
 use src\controllers\Database;
 $bdd = Database::getConnection();
 
+
 // Vérifie si les données du formulaire existent
-if (isset($_POST['nom-Art'], $_POST['date-Rep'], $_POST['heure-Rep'])) {
-    $nom = $_POST['nom-Art'];
-    $date = $_POST['date-Rep'];
-    $heure = $_POST['heure-Rep'];
-    $duree = $_POST["duree-Rep"];
-    $arrive = $_POST["heure-arrivé"];
+
+$nom = $_POST['nom-Art'];
+$date = $_POST['date-Rep'];
+$heure = $_POST['heure-Rep'];
+$duree = $_POST["duree-Rep"];
+$arrive = $_POST["heure-arrivé"];
 
 
-    // Stocke les données dans la session
-    $_SESSION["nom-art-spec"] = $nom;
-    $_SESSION["date-art-spec"] = $date;
-    $_SESSION["heure-art-spec"] = $heure;
-    $_SESSION["duree-rep"] = $duree;
-    $_SESSION["heure-arrivé"] = $arrive;
+// Stocke les données dans la session
+$_SESSION["nom-art-spec"] = $nom;
+$_SESSION["date-art-spec"] = $date;
+$_SESSION["heure-art-spec"] = $heure;
+$_SESSION["duree-rep"] = $duree;
+$_SESSION["heure-arrivé"] = $arrive;
+
+header("Location: Create_Spec2");
 
 
-    // Vérifie si le groupe existe dans la base de données
-    $reqType = $bdd->prepare('SELECT nomG FROM GROUPE WHERE nomG = :nom');
-    $reqType->bindParam(":nom", $nom, PDO::PARAM_STR);
-    $reqType->execute();
 
-    $resultat = $reqType->fetch();
-
-    if ($resultat) {
-        // Le groupe existe : redirige vers la page suivante
-        header("Location: Create_Spec2");
-        exit;
-    } else {
-        // Le groupe n'existe pas : redirige avec un indicateur d'erreur
-        $_SESSION["mauvais_art"] = true;
-        header("Location: Create_Spec");
-        exit;
-    }
-} else {
-    echo "Erreur : tous les champs requis n'ont pas été remplis.";
-}
 ?>
