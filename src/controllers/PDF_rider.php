e3266d63b1e3a8646d332ec8567ede73abea2d94
manglij <nicolas.lepage41@gmail.com>
2025-02-25 12:40:11 +0100

    Deuxiemme

--- src/controllers/PDF_rider.php
+++ src/controllers/PDF_rider.php
@@ -11,10 +11,32 @@ $donnees = $reqType->fetch();
 
 $pdf = new FPDF();
 $pdf->AddPage();
+$pdf->SetTitle('Fiche_rider');
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,'Concert du '.$donnees['dateC']);
+$pdf->Ln();
 $pdf->Cell(40,10,'Nom du groupe : '.$donnees['nomG']);
+$pdf->Ln();
 $pdf->Cell(40,10,'Nom de la salle : '.$donnees['nomS']);
+$pdf->Ln(100);
+$pdf->Cell(40,10,'Materiaux : ');
+$pdf->Ln();
+
+$reqMat = $bdd->prepare('SELECT * FROM MATERIEL NATURAL JOIN BESOIN WHERE idC = :id');
+$reqMat->bindParam(":id", $idC, PDO::PARAM_STR);
+$reqMat->execute();
+$mat = $reqMat->fetch();
+$header = array('Nom', 'Type', 'Quantité');
+foreach($header as $col)
+    $pdf->Cell(40,7,$col,1);
+$pdf->Ln();
+// Données
+foreach($mat as $row)
+{
+    foreach($row as $col)
+        $pdf->Cell(40,6,$col,1);
+    $pdf->Ln();
+}
 
 $pdf->Output();
 ?>
