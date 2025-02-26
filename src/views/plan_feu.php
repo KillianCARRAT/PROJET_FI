7382f209146fb36fbe4ec39fed1ee02994e71c77
moucmoi <noatoimoi@gmail.com>
2025-02-26 08:43:38 +0100

    changement mineur

--- src/views/plan_feu.php
+++ src/views/plan_feu.php
@@ -16,8 +16,8 @@ $idC = $_GET['concert'];
         <!-- Zone des objets à droite -->
         <div class="objects">
             <?php
-            $reqB = $bdd->prepare('SELECT nomM, typeM, nbBesoin FROM MATERIEL NATURAL JOIN BESOIN WHERE idC = 1');
-            //$reqB->bindParam(":idC", $idC, PDO::PARAM_STR);
+            $reqB = $bdd->prepare('SELECT nomM, typeM, nbBesoin FROM MATERIEL NATURAL JOIN BESOIN WHERE idC = :idC');
+            $reqB->bindParam(":idC", $idC, PDO::PARAM_STR);
             $reqB->execute();
             while ($mat = $reqB->fetch()) { 
                 if ($reqB){
