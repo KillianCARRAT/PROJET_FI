b46fd190f1e477ed936cbb1d9bf1e360f3174f00
Fonteny Noa <143808893+moucmoi@users.noreply.github.com>
2025-02-27 10:27:13 +0100

    Changer stockage images (#147)
    * modification stockage des images
    
    * modification stockage des images

--- src/views/affichage_plan_feu.php
+++ src/views/affichage_plan_feu.php
@@ -1,26 +1,26 @@
 <?php
-
-$lesCSS=["image_feu"];
+$lesCSS = ["image_feu"];
 require_once 'head.php';
-$nom=$_GET["concert"];
-$image=__DIR__."/../../public/assets/img_capture/".$nom.".png";
 
-if(file_exists($image)){
-    $image = preg_replace('/^.*\.\.\/\.\.\//', '../../', $image);
-    error_log($image);
+$nom = $_GET["concert"];
+$image = BASE_URL."/src/controllers/PDF/" . $nom . ".png";
+$imagefe=__DIR__."/../controllers/PDF/" . $nom . ".png";
+
+error_log($imagefe);
+
+if (file_exists($imagefe)) {
     print '<body id="body">';
     print '<img src="'.$image.'" alt="texte alternatif" />';
     print '<section id="section">';
     ?>
+
     <button class="bouton" onclick="window.location = '/Ac_Art'">Retour</button>
+
     <?php
-    print '<a href="/plan_feu?concert='.$nom.'"><button class="bouton">Modifier</button></a>';
+    print '<a href="/plan_feu?concert=' . $nom . '"><button class="bouton">Modifier</button></a>';
     print '</section>';
     print '</body>';
+} else {
+    header("Location:/plan_feu?concert=" . $nom);
 }
-else{
-    header("Location:/plan_feu?concert=".$nom);
-}
-
-
 ?>
