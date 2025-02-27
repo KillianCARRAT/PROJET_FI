dba44d894be8c4bf5784493f67758eed94f8c9f2
moucmoi <noatoimoi@gmail.com>
2025-02-27 10:26:22 +0100

    modification stockage des images

--- src/controllers/sauvegarder_Sreen.php
+++ src/controllers/sauvegarder_Sreen.php
@@ -1,18 +1,14 @@
 <?php
-
-
 $data = json_decode(file_get_contents("php://input"), true);
 
 $imageData = $data["image"];
-$nom=$data["idC"];
+$nom = $data["idC"];
 
-$imageData = $data["image"];
 $imageData = str_replace("data:image/png;base64,", "", $imageData);
 $imageData = str_replace(" ", "+", $imageData);
+
 $decodedImage = base64_decode($imageData);
 
-
-
-$fileName = __DIR__."/../../public/assets/img_capture/".$nom.".png";
+$fileName = __DIR__ . "/PDF/" . $nom . ".png";
 file_put_contents($fileName, $decodedImage);
 ?>
