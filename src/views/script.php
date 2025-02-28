c85041edbe854726fa8e4c432ad923ef4de7abd1
KillianCARRAT <143808966+KillianCARRAT@users.noreply.github.com>
2025-02-28 13:48:12 +0100

    Reglage bouton cacher montrer mdp (#156)

--- src/views/script.php
+++ src/views/script.php
@@ -1,6 +1,6 @@
 <script
     <?php foreach ($lesJS as $js): ?>
-src = "<?= BASE_URL; ?>/public/assets/js/<?php echo $js; ?>.js"
+src = "../../public/assets/js/<?php echo $js; ?>.js"
     <?php endforeach; ?>
 
 >
