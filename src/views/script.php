c3629d7f4c402199d5982abd0225aca714d7892b
marin <marin.chesneau31@gmail.com>
2025-02-28 13:47:29 +0100

    commit final

--- src/views/script.php
+++ src/views/script.php
@@ -1,6 +1,6 @@
 <script
     <?php foreach ($lesJS as $js): ?>
-src = "<?= BASE_URL; ?>/public/assets/js/<?php echo $js; ?>.js"
+src = "../../public/assets/js/<?php echo $js; ?>.js" defer
     <?php endforeach; ?>
 
 >
